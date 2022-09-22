6d0761482b51e7c00433c3a95893a44db26026bb
M.Shahzaib <114110117+dj1j2j3k@users.noreply.github.com>
2022-09-22 07:58:02 -0700

    ghh

---
+++ script.js
@@ -0,0 +1,55 @@
+let active = document.querySelector(".active")
+let active2 = document.querySelector(".active2")
+let active3 = document.querySelector(".active3")
+let active4 = document.querySelector(".active4")
+active.addEventListener('click',() =>{
+    active.style.background = "white"
+    active.style.color = "black"
+    active.style.radius ="4px"
+    active2.style.background = "black"
+    active2.style.color = "white"
+    active3.style.background = "black"
+    active3.style.color = "white"
+    active4.style.background = "black"
+    active4.style.color = "white"
+})
+active2.addEventListener('click',() =>{
+    active2.style.background = "white"
+    active2.style.color = "black"
+    active2.style.radius ="4px"
+    active.style.background = "black"
+    active.style.color = "white"
+    active3.style.background = "black"
+    active3.style.color = "white"
+    active4.style.background = "black"
+    active4.style.color = "white"
+})
+active3.addEventListener('click',() =>{
+    active3.style.background = "white"
+    active3.style.color = "black"
+    active3.style.radius ="4px"
+    active2.style.background = "black"
+    active2.style.color = "white"
+    active.style.background = "black"
+    active.style.color = "white"
+    active4.style.background = "black"
+    active4.style.color = "white"
+})
+active4.addEventListener('click',() =>{
+    active4.style.background = "white"
+    active4.style.color = "black"
+    active4.style.radius ="4px"
+    active.style.background = "black"
+    active.style.color = "white"
+    active2.style.background = "black"
+    active2.style.color = "white"
+    active3.style.background = "black"
+    active3.style.color = "white"
+})
+if (active2.style.background == "white"){
+    // active.style.background == "cornflowerblue"
+    // active.addEventListener = 
+}
+// else{
+
+// }
---
+++ script2.js
@@ -0,0 +1,11 @@
+let home = document.getElementById("#home")
+let active = document.querySelector(".active")
+let active2 = document.querySelector(".active2")
+let active3 = document.querySelector(".active3")
+let active4 = document.querySelector(".active4")
+home.addEventListener('scroll',() =>{
+    active.style.background = "white"
+    active.style.color = "cornflowerblue"
+    // alert("this page is scrolled")
+
+})
